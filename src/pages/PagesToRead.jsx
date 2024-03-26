41243b6f43cba23cef1c0b9c3c0466d80a039f66
hasanposh <hmasif147@gmail.com>
2024-03-27 01:04:31 +0600

    chart complete

--- src/pages/PagesToRead.jsx
+++ src/pages/PagesToRead.jsx
@@ -7,19 +7,22 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
 import { getReadBooks } from "../utility/localStorage";
-const PagesToRead = () => {
+import PropTypes from "prop-types";
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+const PagestoRead = () => {
   const data = getReadBooks();
+  console.log(data);
   const getPath = (x, y, width, height) => {
     return `M${x},${y + height}C${x + width / 3},${y + height} ${
       x + width / 2
     },${y + height / 3}
-    ${x + width / 2}, ${y}
-    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
-      x + width
-    }, ${y + height}
-    Z`;
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${
+      y + height
+    } ${x + width}, ${y + height}
+        Z`;
   };
   const TriangleBar = (props) => {
     const { fill, x, y, width, height } = props;
@@ -27,11 +30,9 @@ const PagesToRead = () => {
     return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
   };
   return (
-    <div style={{ width: "100%", height: "400px" }}>
+    <div style={{ width: "100%", height: "600px" }}>
       <ResponsiveContainer>
         <BarChart
-          width={500}
-          height={300}
           data={data}
           margin={{
             top: 20,
@@ -42,15 +43,18 @@ const PagesToRead = () => {
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="bookName" />
-          <YAxis dataKey="totalPages" />
+          <YAxis />
           <Bar
-            dataKey="uv"
+            dataKey="totalPages"
             fill="#8884d8"
             shape={<TriangleBar />}
             label={{ position: "top" }}
           >
             {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              <Cell
+                key={`cell-${index}`}
+                fill={colors[index % colors.length]}
+              />
             ))}
           </Bar>
         </BarChart>
@@ -59,4 +63,13 @@ const PagesToRead = () => {
   );
 };
 
-export default PagesToRead;
+PagestoRead.propTypes = {
+    fill: PropTypes.element,
+    x: PropTypes.element,
+    y: PropTypes.element,
+    width: PropTypes.element,
+    height: PropTypes.element,
+    
+};
+
+export default PagestoRead;
